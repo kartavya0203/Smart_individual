958c15ec892bb2add50fc790be52cbb824de4c49
kartavya0203 <kartavyavaland@gmail.com>
2024-09-03 00:31:03 +0530

    Url Updated

--- Backend/SmartFarm/urls.py
+++ Backend/SmartFarm/urls.py
@@ -20,6 +20,8 @@ from SmartFarmapp.views import CreateViewset
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from django.http import HttpResponseRedirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +29,8 @@ urlpatterns = [
     path('api/token/',TokenObtainPairView.as_view(),name='get_token'),
     path('api/token/refresh/',TokenRefreshView.as_view(),name='refresh_token'),
     path('api-auth/',include('rest_framework.urls')),
-    path('api/v1/',include('SmartFarmapp.urls'))
+    path('api/v1/',include('SmartFarmapp.urls')),
+     path('', lambda request: HttpResponseRedirect('/api/v1/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
